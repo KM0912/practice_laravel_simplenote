c39c30c4bfb14fd576bd662b560b427660837d93
KM0912 <61630275+KM0912@users.noreply.github.com>
2022-02-08 23:18:26 +0900

    タグ機能の実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -55,19 +56,19 @@ class HomeController extends Controller
         // MEMOモデルにDBへ保存する命令を出す
 
         // 同じタグがあるか確認
-        // $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
-        // if( empty($exist_tag['id']) ){
-        //     //先にタグをインサート
-        //     $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
-        // }else{
-        //     $tag_id = $exist_tag['id'];
-        // }
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        if( empty($exist_tag['id']) ){
+            //先にタグをインサート
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        }else{
+            $tag_id = $exist_tag['id'];
+        }
         //タグのIDが判明する
         // タグIDをmemosテーブルに入れてあげる
         $memo_id = Memo::insertGetId([
             'content' => $data['content'],
              'user_id' => $data['user_id'], 
-            //  'tag_id' => $tag_id,
+             'tag_id' => $tag_id,
              'status' => 1
         ]);
         
@@ -90,13 +91,20 @@ class HomeController extends Controller
         ->where('status', 1)
         ->orderBy('updated_at', 'DESC')
         ->get();
-        return view('edit', compact('memo', 'memos', 'user'));
+
+        // タグ取得
+        $tags = Tag::where('user_id', $user['id'])->get();
+
+        return view('edit', compact('memo', 'memos', 'user', 'tags'));
     }
 
     public function update(Request $request, $id)
     {
         $inputs = $request->all();
-        Memo::where('id', $id)->update(['content' => $inputs['content']]);
+        Memo::where('id', $id)->update([
+            'content' => $inputs['content'],
+            'tag_id' => $inputs['tag_id'],
+        ]);
         // リダイレクト処理
         return redirect()->route('home');
     }
